39bfc39ee5ea7405650745125fad8ebc2212e884
S.H.Kim <49853036+skok1025@users.noreply.github.com>
2025-12-15 01:17:15 +0900

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,                         // 외부 접속 허용
+    port: 5273,                         // 원하는 포트
+    allowedHosts: ['shkim3000.cafe24.com'],   // 접속 허용 도메인
+  },
 })
